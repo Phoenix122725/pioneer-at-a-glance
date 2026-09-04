2327cf5098fe2bd7abf2b14ecc72d1bccc9c7a27
Phoenix122725 <thelinquicraftbar@gmail.com>
2026-09-03 22:50:46 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pioneer-at-a-glance-commercial-beta2-9';
+const CACHE='pioneer-at-a-glance-commercial-beta2-12';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
