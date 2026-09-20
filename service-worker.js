0c3ce9d6d121894965e0e83482b2cc87510b8252
Phoenix122725 <thelinquicraftbar@gmail.com>
2026-09-19 20:03:11 -0700

    Update cache version and add data-safety.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='pioneer-at-a-glance-commercial-beta2-22';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='pioneer-at-a-glance-commercial-beta2-23-data-safety';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data-safety.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r})))});
